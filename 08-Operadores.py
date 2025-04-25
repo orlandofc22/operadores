20a5b7407f62fbcd86c25856fb3aa19cf1f4ca67
orlandofc22 <orlandofigueroa4@hotmail.com>
2025-04-24 21:24:11 -0500

    primer comentario

---
+++ 08-Operadores.py
@@ -0,0 +1,14 @@
+x = 6
+y = 2
+
+print(f"{x} mas {y} es igual a {x+y}")
+print(f"{x} menos {y} es igual a {x-y}")
+print(f"{x} multiplicado por {y} es igual a {x*y}")
+print(f"{x} dividido entre {y} es igual a {x/y}")
+
+z = 7
+
+print(f"{z} dividido al piso {y} es igual a {z//y}")
+print(f"{z} modulo de {y} es igual a {z%y}")
+print(f"{x} elevado a {y} es igual a {x**y}")
+print(f"La raíz cuadra de {x} es a {x**0.5}")
